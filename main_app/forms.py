1854ab94401052f8711b2597a676f00fa31c0b37
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-22 14:57:34 -0700

    feat: add MCDatepicker package to the detail.html template to allow users to easily select dates for a better user experience

--- main_app/forms.py
+++ main_app/forms.py
@@ -7,10 +7,11 @@ class FeedingForm(forms.ModelForm):
         fields = ['date', 'meal']
         widgets = {
             'date': forms.DateInput(
-                format=('%Y-%m-%d'),
+                
                 attrs={
                     'placeholder': 'Select a date',
-                    'type': 'date'
+                    'type': 'date',
+                    'id': 'id_date'
                 }
             ),
         }
