490694daab7cd10da4362186fdb0abb8a16da63c
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-22 10:44:00 -0700

    feat: add signup function in main_app/views.py and signup.html template

--- main_app/views.py
+++ main_app/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Cat, Toy
 from .forms import FeedingForm
 from django.contrib.auth.views import LoginView
 
 
-
 class Home(LoginView):
     template_name = 'home.html'
-
 
 def about(request):
     return render(request, 'about.html')
@@ -99,3 +99,28 @@ def remove_toy(request, cat_id, toy_id):
     # Remove the toy from the cat
     cat.toys.remove(toy)
     return redirect('cat-detail', cat_id=cat.id)
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('cat-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+    # Same as: 
+    # return render(
+    #     request, 
+    #     'signup.html',
+    #     {'form': form, 'error_message': error_message}
+    # )
